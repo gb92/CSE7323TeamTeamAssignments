f944667a44d290b31830b4371999880b3a8905e8
ch484-mac7 <chc484-mac@chc484-macs-Mac-mini-3.local>
2014-02-12 16:47:30 -0600

    Initial Commit

---
+++ Assignment2/Assignment2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Assignment2ViewController.h
+//  Assignment2
+//
+//  Created by ch484-mac7 on 2/12/14.
+//  Copyright (c) 2014 ch484-mac7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Assignment2ViewController : UIViewController
+
+@end
