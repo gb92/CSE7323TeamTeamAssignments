c7045e67734daf1ea8e715b14e2ff83691e2f7f5
ch484-mac7 <chc484-mac@chc484-macs-Mac-mini-3.local>
2014-03-01 15:36:51 -0600

    completed all items except stair counting

--- Assignment3/Assignment3/Assignment3ViewController.h
+++ Assignment3/Assignment3/Assignment3ViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface Assignment3ViewController : UIViewController
+@interface Assignment3ViewController : UIViewController <UITextFieldDelegate>
 
 @end
--- Assignment3/Assignment3/Assignment3ViewController.m
+++ Assignment3/Assignment3/Assignment3ViewController.m
@@ -19,13 +19,15 @@
 @property (weak, nonatomic) IBOutlet UIProgressView *stepsToDailyGoalProgressBar;
 @property (weak, nonatomic) IBOutlet UILabel *stairsClimbedLabel;
 
+
 @property (strong,nonatomic) CMMotionActivityManager *cmActivityManager;
-@property (strong,nonatomic) CMDeviceMotion *cmDiviceMotionManager;
 @property (strong,nonatomic) CMStepCounter *cmStepCounter;
 @property (strong,nonatomic) NSNumber *dailyStepGoal;
 
 @property (strong, nonatomic) NSString *currentActivityLabelText;
 
+@property long numberOfStepsToday;
+@property long numberOfSteps;
 @end
 
 @implementation Assignment3ViewController
@@ -33,11 +35,9 @@
 
 -(NSString *) currentActivityLabelText
 {
-    if(!_currentActivityLabelText)
-    {
+    if(!_currentActivityLabelText){
         _currentActivityLabelText=@"Current Activity: %@";
     }
-    
     return _currentActivityLabelText;
 }
 
@@ -61,6 +61,7 @@
 -(NSNumber*)dailyStepGoal{
     if(!_dailyStepGoal){
         _dailyStepGoal = @(100);
+        [self.dailyStepGoalTextField setText:[NSString stringWithFormat:@"%ld", [_dailyStepGoal longValue]]];
     }
     return _dailyStepGoal;
 }
@@ -70,13 +71,52 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
+    self.dailyStepGoalTextField.delegate=self;
+    
+    NSDate *now = [NSDate date];
+    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    
+    unsigned unitFlags= NSHourCalendarUnit|NSMinuteCalendarUnit|NSSecondCalendarUnit;
+
+    
+    NSDateComponents *components = [calendar components:unitFlags fromDate:now];
+    
+    long timeToRemove=-1*([components hour]*60*60 + [components minute]*60 + [components second]);
+    NSLog(@"timeToRemove: %ld", timeToRemove);
+    NSDate *today = [NSDate dateWithTimeInterval:timeToRemove sinceDate:now];
+    NSDate *yesterday = [NSDate dateWithTimeInterval:-60*60*24 sinceDate:today];
+    
+    NSDateFormatter *formatter=[[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"MM-dd-YYYY hh:mm:ss"];
+    NSLog(@"NSDate today: %@",[formatter stringFromDate:today]);
+    NSLog(@"NSDate yesterday: %@", [formatter stringFromDate:yesterday]);
+    NSLog(@"NSDate now: %@", [formatter stringFromDate:now]);
+    
+    [self.cmStepCounter queryStepCountStartingFrom:yesterday to:today toQueue:[NSOperationQueue mainQueue]
+                                       withHandler:^(NSInteger numberOfSteps, NSError *error) {
+                                            self.stepsYesterdayLabel.text = [NSString stringWithFormat:@"Steps Yesterday: %ld",(long)numberOfSteps];
+                                        
+                                       }];
+    
+    [self.cmStepCounter queryStepCountStartingFrom:today to:now toQueue:[NSOperationQueue mainQueue]
+                                       withHandler:^(NSInteger numberOfSteps, NSError *error) {
+                                           self.numberOfStepsToday=numberOfSteps;
+                                           [self.stepsTodayLabel setText:[NSString stringWithFormat:@"Steps Today: %ld",(long)numberOfSteps]];
+                                           [self.stepsToDailyGoalProgressBar setProgress: (numberOfSteps)/[self.dailyStepGoal floatValue]];
+                                       }];
+    
+    
     
     [self.cmStepCounter startStepCountingUpdatesToQueue:[NSOperationQueue mainQueue]
                                                updateOn:1
                                             withHandler:^(NSInteger numberOfSteps, NSDate *timestamp, NSError *error) {
                                                 if(!error){
-                                                    [self.stepsToDailyGoalProgressBar setProgress: numberOfSteps/[self.dailyStepGoal floatValue]];
-                                                    self.stepsTodayLabel.text = [NSString stringWithFormat:@"Steps Today: %ld",(long)numberOfSteps];
+                                                    
+                                                    self.numberOfSteps=(long)numberOfSteps;
+                                                    
+                                                    [self.stepsToDailyGoalProgressBar setProgress: (self.numberOfStepsToday+numberOfSteps)/[self.dailyStepGoal floatValue]];
+                                                    self.stepsTodayLabel.text = [NSString stringWithFormat:@"Steps Today: %ld",(long)(self.numberOfStepsToday+numberOfSteps)];
                                                 }
                                             }];
     
@@ -103,6 +143,23 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)updateDailyGoal:(id)sender {
+    UITextField *dailyGoalTextField=(UITextField*)sender;
+    
+    self.dailyStepGoal=[[NSNumber alloc] initWithInteger:dailyGoalTextField.text.integerValue];
+    
+}
+
+
+
+- (IBAction)backgroundTap:(id)sender {
+    [self.dailyStepGoalTextField resignFirstResponder];
+    
+    self.dailyStepGoal=[[NSNumber alloc] initWithInteger:self.dailyStepGoalTextField.text.integerValue];
+    
+    [self.stepsToDailyGoalProgressBar setProgress: (self.numberOfStepsToday+self.numberOfSteps)/[self.dailyStepGoal floatValue]];
 }
 
 @end
